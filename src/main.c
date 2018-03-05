da6ae14a7bf0a579c26de943c19cc7a875a8fb83
Martin Januario <martin.januario@epitech.eu>
2018-03-05 20:58:36 +0000

    Init + clean lib

--- src/main.c
+++ src/main.c
@@ -10,31 +10,39 @@
 #include "parsing.h"
 #include "extern.h"
 
-static void prompt_help(void)
+static int prompt_help(void)
 {
 	printf("USAGE\n\t./philo -p nbr_p -e nbr_e\n\n");
 	printf("DESCRIPTION\n\t-p nbr_p  number of philosophers");
 	printf("           \n\t-e nbr_e  maximum number times a philosopher "
 		"eats before exiting the program\n");
+	return (1);
 }
 
 static int help_check(int argc, char **argv)
 {
-	if (argc == 1)
-		prompt_help();
-	for (int idx = 1; idx < argc; ++idx) {
+	int ret = 0;
+
+	if (argc == 1 || argc > 5)
+		ret = prompt_help();
+	for (int idx = 1; idx < argc && argc > 5; ++idx) {
 		if (!strcmp(argv[idx], "--help") || !strcmp(argv[idx], "-h")) {
-			prompt_help();
+			ret = prompt_help();
 			break;
 		}
 	}
-	return (0);
+	return (ret);
 }
 
 int main(int argc, char **argv)
 {
+	int ret = 0;
+
 	if (help_check(argc, argv))
 		return (0);
-//	RCFStartup(argc, argv);
-	return (parsing(argc, argv));
+	RCFStartup(argc, argv);
+	ret = parsing(argc, argv);
+	if (ret != 84)
+		RCFCleanup();
+	return (ret);
 }
